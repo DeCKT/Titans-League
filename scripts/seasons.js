0efa6550c75a5183d57aedcb8429696ccc20b00f
DeCKT <dseckton@gmail.com>
2025-02-08 16:38:28 -0700

    moving to vite

--- scripts/seasons.js
+++ scripts/seasons.js
@@ -1,28 +1,23 @@
+import games from '../data/games.json'
+import groups from '../data/groups.json'
+import leagues from '../data/leagues.json'
+import maps from '../data/maps.json'
+import matches from '../data/matches.json'
+import players from '../data/players.json'
+import seasons from '../data/seasons.json'
+
 const page_header = document.querySelector("#page_header");
 const league_selector = document.querySelector("#league_selector");
+
 
 const params = new URLSearchParams(window.location.search);
 
 const seasonParam = params.get("season");
 
-async function getSeasonsData() {
-  try {
-    const res = await fetch("../data/seasons.json");
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-getSeasonsData().then((seasonsData) => {
-  const selectedSeason = seasonsData.find(
-    (season) => season.name === `Season ${seasonParam}`
-  );
-  console.log(selectedSeason.leagues);
-});
 
 if (season) {
   document.title = `Season ${season} | T90 Titans League`;
   page_header.textContent = `Season ${season}`;
 }
+
+console.log(games)
