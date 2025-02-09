098eb0eb56229b97446f1c6a8e8e05dabeb4166c
DeCKT <dseckton@gmail.com>
2025-02-09 13:51:08 -0700

    finalizing navbar and beginning player page

---
+++ scripts/navbar.js
@@ -0,0 +1,33 @@
+const seasonSelector = document.querySelector('#seasonSelector')
+const dropdownSvg = document.querySelector('#dropdownSvg')
+const dropdownMenu = document.querySelector('#dropdownMenu')
+
+const seasonSelectorButton = document.querySelector('#seasonSelectorButton')
+
+let buttonMenuOpen = false;
+
+seasonSelector.addEventListener('mouseover', () => {
+  if (!buttonMenuOpen) {
+    dropdownSvg.classList.add('rotate-180')
+    dropdownMenu.classList.remove('-translate-y-full')
+  }
+})
+
+seasonSelector.addEventListener('mouseout', () => {
+  if (!buttonMenuOpen) {
+    dropdownSvg.classList.remove('rotate-180')
+    dropdownMenu.classList.add('-translate-y-full')
+  }
+})
+
+seasonSelectorButton.addEventListener('click', () => {
+  if (buttonMenuOpen) {
+    dropdownSvg.classList.remove('rotate-180')
+    dropdownMenu.classList.add('-translate-y-full')
+    buttonMenuOpen = false;
+  } else {
+    dropdownSvg.classList.add('rotate-180')
+    dropdownMenu.classList.remove('-translate-y-full')
+    buttonMenuOpen = true;
+  }
+})
---
+++ scripts/players.js
@@ -0,0 +1,69 @@
+import players from '../data/players.json'
+
+const charMap = {
+  'Ø': 'O', 'ø': 'o',
+  'Æ': 'AE', 'æ': 'ae',
+  'Å': 'A', 'å': 'a',
+  'É': 'E', 'é': 'e',
+  'Á': 'A', 'á': 'a',
+  'Í': 'I', 'í': 'i',
+  'Ó': 'O', 'ó': 'o',
+  'Ú': 'U', 'ú': 'u',
+  'Ñ': 'N', 'ñ': 'n',
+  'Ü': 'U', 'ü': 'u',
+  'Ç': 'C', 'ç': 'c',
+  'Š': 'S', 'š': 's',
+  'Ž': 'Z', 'ž': 'z'
+};
+
+const countries = {
+  NO: "Norway",
+  DE: "Germany",
+  RS: "Serbia",
+  BR: "Brazil",
+  UA: "Ukraine",
+  VN: "Vietnam",
+  CA: "Canada",
+  FI: "Finland",
+  CN: "China",
+  PL: "Poland",
+  IN: "India",
+  RU: "Russia",
+  AT: "Austria",
+  AR: "Argentina",
+  ES: "Spain"
+}
+
+function getAge(dateOfBirth) {
+  const birthDate = new Date(dateOfBirth);
+  const today = new Date();
+
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const hasBirthdayPassed =
+      today.getMonth() > birthDate.getMonth() ||
+      (today.getMonth() === birthDate.getMonth() && today.getDate() >= birthDate.getDate());
+
+  if (!hasBirthdayPassed) {
+      age--;
+  }
+
+  return age;
+}
+
+
+const playersContainer = document.querySelector('#playersContainer')
+
+players.forEach((player) => {
+
+  const normalizedName = player.name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[ØøÆæÅåÉéÁáÍíÓóÚúÑñÜüÇçŠšŽž]/g, match => charMap[match] || match);
+
+  let age = getAge(player.dob)
+
+  playersContainer.innerHTML += `
+    <li data-info="${player.ign} ${player.name} ${player.country} ${countries[player.country]} ${player.team || ''} ${normalizedName === player.name ? '' : normalizedName}" style="background-image: url('${player.img ?? '../assets/playerunknown.png'}')" class="bg-no-repeat bg-cover w-3xs aspect-3/4">
+      <div><img src="https://flagsapi.com/${player.country}/flat/24.png" /></div>
+      <div>${player.ign}</div>
+      <div></div>
+    </li>
+  `
+})
