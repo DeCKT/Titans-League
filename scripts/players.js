e163c14e37f0ebefbcd5df033288830cba0f8ff9
DeCKT <dseckton@gmail.com>
2025-02-09 19:55:21 -0700

    finalizing navbar and implementing search into player page

--- scripts/players.js
+++ scripts/players.js
@@ -84,14 +84,32 @@ players.forEach((player) => {
     normalizedName === player.name ? "" : normalizedName
   }" style="background-image: url('${
     player.img ?? "../assets/playerunknown.png"
-  }')" class="bg-no-repeat bg-cover w-3xs aspect-3/4">
-      <div class="bg-linear-to-t from-black/80 from-30% to-black/0 w-full h-full">
-        <div><img src="https://flagsapi.com/${
+  }')" class="bg-no-repeat bg-cover w-3xs aspect-3/4 overflow-hidden rounded-md hover:outline-2 hover:outline-sky-50 hover:scale-102 player-card">
+      <div class="bg-linear-to-t from-black/80 from-30% to-black/0 w-full h-full flex flex-col text-center p-2">
+        <div class="flex w-full justify-center gap-2"><img src="https://flagsapi.com/${
           player.country
-        }/flat/24.png" /></div>
-        <div>${player.ign}</div>
-        <div>${player.dob ? age : ""}</div>
+        }/flat/24.png" />${player.team ? `<div>${player.team}</div>` : ''}</div>
+        <div class="mt-auto font-black text-3xl">${player.ign}</div>
+        <div class="font-light text-sm text-sky-50/75 pt-1">${player.name}</div>
+        <div class="font-light text-xs text-sky-50/75 min-h-4">${player.dob ? `${age} years old` : ""}</div>
       </div>
     </li>
   `;
 });
+
+const playerCards = document.querySelectorAll('.player-card')
+
+const searchInput = document.querySelector('#searchInput')
+const searchButton = document.querySelector('#searchButton')
+
+searchButton.addEventListener('click', searchPlayers)
+
+function searchPlayers () {
+  playerCards.forEach((card) => {
+    if (card.dataset.info.toLowerCase().search(searchInput.value.toLowerCase()) >= 0) {
+      card.classList.remove('hidden')
+    } else {
+      card.classList.add('hidden')
+    }
+  })
+}
