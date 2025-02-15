6aa7d1a7f534e6aa87bb4d46b916712e6ddaba31
dseckton <dseckton@gmail.com>
2025-02-14 18:07:02 -0700

    centering bg images

--- scripts/players.js
+++ scripts/players.js
@@ -84,32 +84,41 @@ players.forEach((player) => {
     normalizedName === player.name ? "" : normalizedName
   }" style="background-image: url('${
     player.img ?? "../assets/playerunknown.png"
-  }')" class=" w-full max-w-[calc((100%-0.75rem)/2)] sm:max-w-[calc((100%-1.5rem)/3)] md:max-w-[calc((100%-2.25rem)/4)] lg:max-w-[calc((100%-3rem)/5)] xl:max-w-[calc((100%-3.75rem)/6)] 2xl:max-w-[calc((100%-4.5rem)/7)] bg-no-repeat bg-cover aspect-3/4 duration-100 overflow-hidden rounded-md hover:outline-2 hover:outline-sky-50 hover:scale-102 player-card">
-      <a href="/player?ign=${player.ign}" class="bg-linear-to-t from-black/80 from-30% to-black/0 w-full h-full flex flex-col text-center p-2">
+  }')" class="bg-center w-full max-w-[calc((100%-0.75rem)/2)] sm:max-w-[calc((100%-1.5rem)/3)] md:max-w-[calc((100%-2.25rem)/4)] lg:max-w-[calc((100%-3rem)/5)] xl:max-w-[calc((100%-3.75rem)/6)] 2xl:max-w-[calc((100%-4.5rem)/7)] bg-no-repeat bg-cover aspect-3/4 duration-100 overflow-hidden rounded-md hover:outline-2 hover:outline-sky-50 hover:scale-102 player-card">
+      <a href="/player?ign=${
+        player.ign
+      }" class="bg-linear-to-t from-black/80 from-30% to-black/0 w-full h-full flex flex-col text-center p-2">
         <div class="flex w-full justify-center gap-2"><img src="https://flagsapi.com/${
           player.country
-        }/flat/24.png" />${player.team ? `<div>${player.team}</div>` : ''}</div>
-        <div class="mt-auto font-black text-xl md:text-2xl truncate">${player.ign}</div>
+        }/flat/24.png" />${player.team ? `<div>${player.team}</div>` : ""}</div>
+        <div class="mt-auto font-black text-xl md:text-2xl truncate">${
+          player.ign
+        }</div>
         <div class="font-light text-sm text-sky-50/75 pt-1">${player.name}</div>
-        <div class="font-light text-xs text-sky-50/75 min-h-4">${player.dob ? `${age} years old` : ""}</div>
+        <div class="font-light text-xs text-sky-50/75 min-h-4">${
+          player.dob ? `${age} years old` : ""
+        }</div>
       </a>
     </li>
   `;
 });
 
-const playerCards = document.querySelectorAll('.player-card')
+const playerCards = document.querySelectorAll(".player-card");
 
-const searchInput = document.querySelector('#searchInput')
-const searchButton = document.querySelector('#searchButton')
+const searchInput = document.querySelector("#searchInput");
+const searchButton = document.querySelector("#searchButton");
 
-searchButton.addEventListener('click', searchPlayers)
+searchButton.addEventListener("click", searchPlayers);
 
-function searchPlayers () {
+function searchPlayers() {
   playerCards.forEach((card) => {
-    if (card.dataset.info.toLowerCase().search(searchInput.value.toLowerCase()) >= 0) {
-      card.classList.remove('hidden')
+    if (
+      card.dataset.info.toLowerCase().search(searchInput.value.toLowerCase()) >=
+      0
+    ) {
+      card.classList.remove("hidden");
     } else {
-      card.classList.add('hidden')
+      card.classList.add("hidden");
     }
-  })
+  });
 }
