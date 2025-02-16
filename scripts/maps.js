1c15e697f1c74f7b366d83db87eeb0d6387266ba
DeCKT <dseckton@gmail.com>
2025-02-16 07:55:35 -0700

    rounding out data

--- scripts/maps.js
+++ scripts/maps.js
@@ -4,17 +4,30 @@ const mapsContainer = document.querySelector("#mapsContainer")
 
 maps.forEach((map) => {
 
-  map.types.join(' ')
-
   mapsContainer.innerHTML += `
-    <li data-info="${map.name} ${map.types.join(' ')}" class="w-full max-w-[calc((100%-1.5rem)/3)]  
-sm:max-w-[calc((100%-2.25rem)/4)]  
-md:max-w-[calc((100%-3rem)/5)]  
-lg:max-w-[calc((100%-3.75rem)/6)]  
-xl:max-w-[calc((100%-4.5rem)/7)]  
-2xl:max-w-[calc((100%-5.25rem)/8)] duration-100 rounded-md hover:outline-2 hover:outline-sky-50 hover:scale-102 relative">
+    <li data-info="${map.name} ${map.types.join(' ')}" class="w-full max-w-[calc((100%-1.5rem)/3)] sm:max-w-[calc((100%-2.25rem)/4)] md:max-w-[calc((100%-3rem)/5)] lg:max-w-[calc((100%-3.75rem)/6)] xl:max-w-[calc((100%-4.5rem)/7)] 2xl:max-w-[calc((100%-5.25rem)/8)] duration-100 rounded-md hover:outline-2 hover:outline-sky-50 hover:scale-102 relative map-card">
       <img src="${map.img ? `/assets/maps/${map.img}` : '/assets/maps/cm_generic.png'}">
-      <div class="absolute left-[50%] translate-x-[-50%] bottom-5 py-2 px-1 w-[90%] text-center bg-amber-950">${map.name}</div>
+      <div class="absolute left-[50%] translate-x-[-50%] bottom-5 py-2 px-1 w-[90%] text-center bg-gray-950 border border-x-1 border-y-2 border-sky-50 rounded-sm">${map.name}</div>
     </li>
   `
 })
+
+const mapCards = document.querySelectorAll(".map-card");
+
+const searchInput = document.querySelector("#searchInput");
+const searchButton = document.querySelector("#searchButton");
+
+searchButton.addEventListener("click", searchMaps);
+
+function searchMaps() {
+  mapCards.forEach((card) => {
+    if (
+      card.dataset.info.toLowerCase().search(searchInput.value.toLowerCase()) >=
+      0
+    ) {
+      card.classList.remove("hidden");
+    } else {
+      card.classList.add("hidden");
+    }
+  });
+}
